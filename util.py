4563ce2e1b61ce1d5c2490f6905f98d2223b75a1
Zhiliang Gong <zhiliang.gong@gmail.com>
2017-10-03 20:49:35 -0500

    read data, write predictions

--- util.py
+++ util.py
@@ -4,7 +4,6 @@ import numpy as np
 from sklearn import metrics, preprocessing, linear_model
 
 def read_data():
-    print('loading data...')
     training_data = pd.read_csv('/Users/zhilianggong/Code/numerai/data/numerai_training_data.csv', header = 0)
     tour_data = pd.read_csv('/Users/zhilianggong/Code/numerai/data/numerai_tournament_data.csv', header = 0)
 
@@ -15,3 +14,8 @@ def read_data():
     ids = tour_data['id']
 
     return X, Y, x_pred, ids
+
+def write_prediction_csv(pred_array, ids):
+    results_df = pd.DataFrame(data = { 'probability': pred_array })
+    joined = pd.DataFrame(ids).join(results_df)
+    joined.to_csv('/Users/zhilianggong/Code/numerai/predictions/predictions.csv', index = False)
